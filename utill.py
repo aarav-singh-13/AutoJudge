73eadab3379e3a879e4178d52d16e064400d8d93
Aarav Singh <Aarav.as136@gmail.com>
2026-01-04 12:12:18 +0530

    final change

--- utill.py
+++ utill.py
@@ -1,7 +1,7 @@
 def score_to_class(score):
-    if score <= 3.3:
+    if score <= 3.33:
         return 'easy'
-    elif score >= 6.6:
+    elif score >= 6.66:
         return 'hard'
     else:
         return 'medium'
